20fd6c42764829b4129eb2e0ad51e3d734d16b4a
Arif <arifajif@gmail.com>
2022-06-01 22:24:50 +0700

    tugas 9

---
+++ tugas9.js
@@ -0,0 +1,12 @@
+function panggilobject() {
+    var guru = {
+        nama : "Iwan",
+        umur : 27,
+        Status : "Honorer",
+        alamat : "Bojong",
+    }
+for (var x in guru){
+    console.log(guru[x])
+}
+}
+panggilobject();
